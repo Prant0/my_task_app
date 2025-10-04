5e92fc62baaa257780fc39dfe6b7628a94f17dd4
prant0 <shirajpranto89@gmail.com>
2025-10-05 00:35:30 +0600

    5oct

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:task_manager/app_theme.dart';
 import 'package:task_manager/features/splash/views/splash_page.dart';
 import 'helper/get_di.dart' as di;
-
+//
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await di.init();
